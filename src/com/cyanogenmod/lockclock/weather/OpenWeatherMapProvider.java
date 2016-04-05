39f0dbd0bb0db60e270c169085bd998a483499bb
beanstown106 <nbenis106@gmail.com>
2016-04-05 11:38:37 -0400

    Use our own APP ID for Open Weather. ***OTHER ROM DEVS, DO NOT CHERRY PICK PLEASE***
    If you would like your own app ID, please visit
    http://openweathermap.org/appid and get your own
    
    ***OTHER ROM DEVS, DO NOT CHERRY PICK PLEASE***
    
    Originally done by @nickdoheryty81 for DU
    Made my own key following the link DEVS its easy as 1,2,3 dont pick!!
    New api key generated by @BeansTown106 for PureNexus

--- src/com/cyanogenmod/lockclock/weather/OpenWeatherMapProvider.java
+++ src/com/cyanogenmod/lockclock/weather/OpenWeatherMapProvider.java
@@ -20,7 +20,7 @@ public class OpenWeatherMapProvider implements WeatherProvider {
     private static final int FORECAST_DAYS = 5;
     private static final String SELECTION_LOCATION = "lat=%f&lon=%f";
     private static final String SELECTION_ID = "id=%s";
-    private static final String APP_ID = "e2b075d68c39dc43e16995653fcd6fd0";
+    private static final String APP_ID = "5d417513d4b73e5986dbc439f9d8f0fd";
 
     private static final String URL_LOCATION =
             "http://api.openweathermap.org/data/2.5/find?q=%s&mode=json&lang=%s&appid="
