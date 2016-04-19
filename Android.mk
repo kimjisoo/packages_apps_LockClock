84b922d471195eeed53467d22e07b983ddc91795
kimjisoo <kimjisoo.android@gmail.com>
2016-04-19 14:21:37 +0900

    Revert "LockClock: do not use GPS if Play Services is available"
    This reverts commit c5a4398fae1116f0db5a6fe2b3a36fb74c4b723d.

--- Android.mk
+++ Android.mk
@@ -24,14 +24,4 @@ LOCAL_PACKAGE_NAME := LockClock
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
 
-# Include res dir from chips
-google_play_dir := ../../../external/google/google_play_services/libproject/google-play-services_lib/res
-res_dir := $(google_play_dir) res
-
-LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dir))
-LOCAL_AAPT_FLAGS := --auto-add-overlay
-LOCAL_AAPT_FLAGS += --extra-packages com.google.android.gms
-
-LOCAL_STATIC_JAVA_LIBRARIES := play
-
 include $(BUILD_PACKAGE)
